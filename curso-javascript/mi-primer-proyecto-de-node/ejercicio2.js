f51241185defac3017073c2bd0f2f72e8aa35f56
Tania Veleizán <103219354+taninaFED@users.noreply.github.com>
2023-03-23 18:43:33 -0300

    agrego ejercicio 2

---
+++ curso-javascript/mi-primer-proyecto-de-node/ejercicio2.js
@@ -0,0 +1,11 @@
+const nombre = 'Tania';
+const edad = 32;
+const desarrollador = true;
+const fechaDeNacimiento = new Date(1991, 1, 18);
+console.log(fechaDeNacimiento)
+const libroFavorito = {
+    titulo: 'Las bicis del sol',
+    autor: ['Rosa Maria Rey', 'Maria del Carmen Capel'],
+    fecha: 1989,
+    url: 'https://books.google.com.sv/books?id=sOLnvU_T9QwC&lpg=PP1&hl=es&pg=PA97#v=onepage&q&f=true',
+};
